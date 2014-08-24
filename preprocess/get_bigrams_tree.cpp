003fe6e6e1a299fd9cb4daf0a964ba8ec69c5841
Blackbird <alexander.drozd@gmail.com>
2014-08-24 23:58:59 +0900

    load from crs format

--- preprocess/get_bigrams_tree.cpp
+++ preprocess/get_bigrams_tree.cpp
@@ -113,22 +113,47 @@ void write_vector_to_file(std::string name,std::vector<Index> const &  values)
 
 void dump_crs(std::string path_out)
 {
-    std::string str_path = (boost::filesystem::path(path_out) / boost::filesystem::path("bigrams.data")).string();
     std::ofstream file;
+    std::string str_path = (boost::filesystem::path(path_out) / boost::filesystem::path("bigrams.data")).string();
     file.open (str_path);
     if(!file) throw  std::runtime_error("can not open output file "+str_path+" , check the path");
-    for (const auto& first : counters) 
+    for (const auto& first : counters)  //vriting data
     {
         for (const auto& second : first.second) 
         {
-            //if (t.second>0)
-            //file<<first.first-1<<"\t"<<second.first-1<<"\t"<<second.second<<"\n";
             double v=log2((static_cast<double>(second.second)*cnt_words)/(freq_per_id[first.first]*freq_per_id[second.first]));
-            //std::cerr<<"first.first = "<<first.first<<"["<<freq_per_id[first.first]<<"]\t second.first = "<<second.first<<"["<<freq_per_id[second.first]<<"]\n";
             file<<v<<"\n";
         }
-
     }
+    file.close();
+    str_path = (boost::filesystem::path(path_out) / boost::filesystem::path("bigrams.col_ind")).string();
+    file.open (str_path);
+    for (const auto& first : counters)  //vriting columnt indices
+    {
+        for (const auto& second : first.second) 
+        {
+            file<<second.first<<"\n";
+        }
+    }
+    file.close();
+    str_path = (boost::filesystem::path(path_out) / boost::filesystem::path("bigrams.row_ptr")).string();
+    file.open (str_path);
+    Index row_ptr=0;
+    Index id_last=0;
+    for (const auto& first : counters)  //vriting columnt indices
+    {
+        std::cerr<<"first.first = "<<first.first<<"\t count = "<<first.second.size()<<"\n";
+        if (first.first==0) file<<row_ptr<<"\n";
+        else
+            for (Index k=id_last;k<first.first;k++)
+                file<<row_ptr<<"\n";
+            id_last=first.first;
+            row_ptr+=first.second.size();
+    }
+    for (Index k=id_last;k<id_global;k++)
+         file<<row_ptr<<"\n";
+
+    file.close();
 }
 
 int main(int argc, char * argv[])
