7a7294bad8ca17f79adfa0229352fc221e5243ad
Blackbird <alexander.drozd@gmail.com>
2015-02-03 12:38:17 +0900

    why ipynb is here??

--- python/test.py
+++ python/test.py
@@ -7,10 +7,10 @@ import vsmlib
 
 
 m = vsmlib.Model_explicit()
-m.load("/mnt/work/nlp_scratch/BNC_w3/")
+m.load("/mnt/work/nlp_scratch/test/")
 #m.load("/storage/scratch/BNC/")
-m_svd_scipy = vsmlib.Model_svd_scipy(m,300)
-m_svd_scipy.save_to_dir("/mnt/work/nlp_scratch/BNC_w3_svd300/")
+m_svd_scipy = vsmlib.Model_svd_scipy(m,3)
+m_svd_scipy.save_to_dir("/mnt/work/nlp_scratch/test_svd300/")
 
 #m2 = vsmlib.model.Model_numbered()
 #m2.load_from_dir("/storage/scratch/BNC_svd300")
