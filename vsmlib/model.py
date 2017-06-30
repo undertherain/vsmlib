f529722397a8263224738519e154a6ca1eaa7b2a
undertherain <alexander.drozd@gmail.com>
2017-06-30 23:53:27 +0900

    colors moved to format helper

--- vsmlib/model.py
+++ vsmlib/model.py
@@ -12,17 +12,7 @@ import gzip
 import brewer2mpl
 import tables
 import json
-import datetime
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from misc.formathelper import bcolors
 
 
 def normed(v):
