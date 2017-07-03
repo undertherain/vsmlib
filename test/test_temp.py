47106ac32938c4fde034bb9378c0369cea5fc06d
undertherain <alexander.drozd@gmail.com>
2017-07-03 23:15:52 +0900

    clean up matrix

--- test/test_temp.py
+++ test/test_temp.py
@@ -11,3 +11,4 @@ class Tests(unittest.TestCase):
     def test_query_gpus(self):
         model = vsmlib.model.load_from_dir("/storage/embeddings/explicit_BNC_m100_w2_svd_d200")
         self.assertIsInstance(model, object)
+        print(model.name)
