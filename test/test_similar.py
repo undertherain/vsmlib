d35f8270ed36d26e37ad911aa968c09687886532
undertherain <alexander.drozd@gmail.com>
2017-07-06 15:13:53 +0900

    add test similar

---
+++ test/test_similar.py
@@ -0,0 +1,14 @@
+import unittest
+from vsmlib.model import ModelNumbered
+
+path = "/mnt/storage/Data/Embeddings/explicit_BNC_m100_w2_svd_d200"
+
+class Tests(unittest.TestCase):
+
+    def test_similarity(self):
+        model = ModelNumbered()
+        model.load_with_alpha(path)
+        l = model.get_most_similar_words("apple")
+        for w, s in l:
+            print(w,s)
+        self.assertIsInstance(model, object)
