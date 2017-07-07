66fde1102b9815a3b6e029b23044b2a6a48e1258
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-07-08 06:58:54 +0900

    naive corpus loader

---
+++ vsmlib/corpus/corpus.py
@@ -0,0 +1,22 @@
+# convert text to array of indices
+# or should we do this in vocabulary?
+
+import numpy as np
+
+
+def load_as_ids(path, vocabulary, gzipped=None):
+    # user proper tokenizer from cooc
+    # options to ignore sentence bounbdaries
+    # specify what to do with missing words
+    # downcase
+    # replace numbers with special tokens
+    result = []
+    with open(path) as f:
+        for line in f:
+            for token in line.split():
+                result.append(vocabulary.word_to_id(token))
+    return np.array(result, dtype=np.int32)
+
+
+def main():
+    print("test")
