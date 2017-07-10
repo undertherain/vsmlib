28d3c5db71c9e186c6802fcfa06f5149ef328c78
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-07-11 07:57:20 +0900

    add missing test vocab

---
+++ test/test_vocab.py
@@ -0,0 +1,13 @@
+"""Tests for vocabulary module."""
+
+import unittest
+from vsmlib.vocabulary import create_from_dir
+
+path_text = "./test/data/corpora/small"
+
+
+class Tests(unittest.TestCase):
+
+    def test_create_from_dir(self):
+        vocab = create_from_dir(path_text)
+        print ("the:", vocab.get_id("the"))
