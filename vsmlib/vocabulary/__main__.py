883008badcd50b5ccff29457a7715a0e69abcc9c
Alksandr Drozd <alexander.drozd@gmail.com>
2017-07-11 18:37:05 +0900

    source dir as option to vocavulary create

--- vsmlib/vocabulary/__main__.py
+++ vsmlib/vocabulary/__main__.py
@@ -1,5 +1,18 @@
+import argparse
+from vsmlib.vocabulary import create_from_dir
+
+
 def main():
-    print("create vocab")
+    parser = argparse.ArgumentParser(description='Create vocabulary from dir')
+    parser.add_argument("src")
+    parser.add_argument("dst")
+
+    args = parser.parse_args()
+
+    vocab = create_from_dir(args.src)
+    print("the:", vocab.get_id("the"))
+
+    # save to destination
 
 if __name__ == "__main__":
     main()
