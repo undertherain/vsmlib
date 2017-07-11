6d07b9a9c6f6675055cfcd5392d90cb3f0d3d828
Alksandr Drozd <alexander.drozd@gmail.com>
2017-07-11 11:36:24 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name='vsmlib',    # This is the name of your PyPI-package.
+    version='0.1'                          # Update the version number for new releases
+    )
