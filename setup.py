3385dd227d123d2f570977519af07277d4551aef
Alksandr Drozd <alexander.drozd@gmail.com>
2017-07-12 10:27:57 +0900

    add matplitlib

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 import typing as t
 import os
+import shutil
 
 
 _HERE = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
@@ -35,12 +36,28 @@ def parse_requirements(
     return requirements
 
 
-setuptools.setup(
-    name='vsmlib',
-    version='0.1.1',
-    url='http://vsm.blackbird.pw/',
-    classifiers=classifiers,
-    keywords=['NLP', 'linguistics', 'language'],
-    install_requires=parse_requirements(),
-    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*'])
-    )
+def clean(build_directory_name: str = 'build') -> None:
+    """Recursively delete build directory (by default "build") if it exists."""
+    build_directory_path = os.path.join(_HERE, build_directory_name)
+    if os.path.isdir(build_directory_path):
+        shutil.rmtree(build_directory_path)
+
+
+def setup():
+    setuptools.setup(
+        name='vsmlib',
+        version='0.1.2',
+        url='http://vsm.blackbird.pw/',
+        classifiers=classifiers,
+        keywords=['NLP', 'linguistics', 'language'],
+        install_requires=parse_requirements(),
+        packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*'])
+        )
+
+
+def main() -> None:
+    clean()
+    setup()
+
+if __name__ == '__main__':
+    main()
