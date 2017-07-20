d3e9ee848109951b804aa45ebf34b7be087afb17
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-07-20 13:59:35 +0900

    fix path for vocab test

--- test/test_vocab.py
+++ test/test_vocab.py
@@ -3,16 +3,16 @@
 import unittest
 from vsmlib.vocabulary import create_from_dir, Vocabulary
 
-path_text = "./test/data/corpora/small"
+path_text = "./test/data/corpora/plain"
 path_vocab = "./test/data/vocab"
 
 
 class Tests(unittest.TestCase):
 
     def test_create_from_dir(self):
-        vocab = create_from_dir(path_text)
+        vocab = create_from_dir(path_text, min_frequency=1)
         print("the:", vocab.get_id("the"))
-        # vocab.save_to_dir("/tmp/vocab")
+        vocab.save_to_dir("/tmp/vsmlib/vocab")
 
     def test_load_from_dir(self):
         vocab = Vocabulary()
