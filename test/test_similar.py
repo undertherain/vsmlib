0d09c56551370b8bb76850bd002df766321a632c
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-07-20 14:14:42 +0900

    housekeeping

--- test/test_similar.py
+++ test/test_similar.py
@@ -1,7 +1,6 @@
 import unittest
 import vsmlib
 
-# path = "/mnt/storage/Data/Embeddings/explicit_BNC_m100_w2_svd_d200"
 path = "test/data/embeddings/text/plain"
 
 
@@ -9,7 +8,7 @@ class Tests(unittest.TestCase):
 
     def test_similarity(self):
         model = vsmlib.model.load_from_dir(path)
-        sims = model.get_most_similar_words("apple", 12)
+        sims = model.get_most_similar_words("apple", cnt=12)
         for w, s in sims:
             print(w, s)
         self.assertIsInstance(model, object)
