b3315368055472cd4114f851211ca057101bacc3
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-10-11 18:14:29 +0900

    also add current path to modules path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import sys
 import pkg_resources
 
 # sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../'))
 
 #__version__ = pkg_resources.get_distribution('vsmlib').version
