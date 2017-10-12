8e4c4500b392bad766784b2ad0493913be3ae5c0
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-10-12 11:39:15 +0900

    move analogy into submodule

--- test/test_analogies.py
+++ test/test_analogies.py
@@ -12,25 +12,25 @@ class Tests(unittest.TestCase):
     def test_3cosadd(self):
         path_model = "./test/data/embeddings/text/plain_no_file_header"
         model = vsmlib.model.load_from_dir(path_model)
-        vsmlib.benchmarks.analogy.options["dir_root_dataset"] = "./test/data/"
-        vsmlib.benchmarks.analogy.options["path_results"] = "/tmp/vsmlib/analogy"
-        vsmlib.benchmarks.analogy.m = model  # todo this is ugly and should be fixed
-        vsmlib.benchmarks.analogy.run_all("benchmarks")
+        vsmlib.benchmarks.analogy.analogy.options["dir_root_dataset"] = "./test/data/"
+        vsmlib.benchmarks.analogy.analogy.options["path_results"] = "/tmp/vsmlib/analogy"
+        vsmlib.benchmarks.analogy.analogy.m = model  # todo this is ugly and should be fixed
+        vsmlib.benchmarks.analogy.analogy.run_all("benchmarks")
 
     def test_LRcos(self):
         path_model = "./test/data/embeddings/text/plain_with_file_header"
         model = vsmlib.model.load_from_dir(path_model)
-        vsmlib.benchmarks.analogy.options["dir_root_dataset"] = "./test/data/"
-        vsmlib.benchmarks.analogy.options["path_results"] = "/tmp/vsmlib/analogy"
-        vsmlib.benchmarks.analogy.options["name_method"] = "LRCos"
-        vsmlib.benchmarks.analogy.m = model
-        vsmlib.benchmarks.analogy.run_all("benchmarks")
+        vsmlib.benchmarks.analogy.analogy.options["dir_root_dataset"] = "./test/data/"
+        vsmlib.benchmarks.analogy.analogy.options["path_results"] = "/tmp/vsmlib/analogy"
+        vsmlib.benchmarks.analogy.analogy.options["name_method"] = "LRCos"
+        vsmlib.benchmarks.analogy.analogy.m = model
+        vsmlib.benchmarks.analogy.analogy.run_all("benchmarks")
 
     def test_PairDistance(self):
         path_model = "./test/data/embeddings/text/plain_with_file_header"
         model = vsmlib.model.load_from_dir(path_model)
-        vsmlib.benchmarks.analogy.options["dir_root_dataset"] = "./test/data/"
-        vsmlib.benchmarks.analogy.options["path_results"] = "/tmp/vsmlib/analogy"
-        vsmlib.benchmarks.analogy.options["name_method"] = "PairDistance"
-        vsmlib.benchmarks.analogy.m = model
-        vsmlib.benchmarks.analogy.run_all("benchmarks")
+        vsmlib.benchmarks.analogy.analogy.options["dir_root_dataset"] = "./test/data/"
+        vsmlib.benchmarks.analogy.analogy.options["path_results"] = "/tmp/vsmlib/analogy"
+        vsmlib.benchmarks.analogy.analogy.options["name_method"] = "PairDistance"
+        vsmlib.benchmarks.analogy.analogy.m = model
+        vsmlib.benchmarks.analogy.analogy.run_all("benchmarks")
