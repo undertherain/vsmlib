d41002c7da5ab4fdc7aec2ab9c7aee9826cbf72a
Shashwath H A <shashwath94@gmail.com>
2017-10-30 22:29:53 -0400

    NER task added

--- vsmlib/benchmarks/sequence_labeling/load_data.py
+++ vsmlib/benchmarks/sequence_labeling/load_data.py
@@ -1,45 +1,50 @@
 import os
 
-def pos(path):
+def load(path, task):
     dicts = {}
     dicts['labels2idx'] = {}
     dicts['words2idx'] = {}
+
+    if task == 'pos':
+        tag_position = 1
+    else:
+        tag_position = -1
 
     for type in ["train", "test", "valid"] :
         with open(os.path.join(path, type + ".txt")) as f:
             for line in f:
                 if len(line.strip()) is 0 :
                     continue
-                a, b, c, d = line.strip().split()
-                a = a.lower()
-                if a not in dicts['words2idx']:
-                    dicts['words2idx'][a] = len(dicts['words2idx'])
-                if b not in dicts['labels2idx']:
-                    dicts['labels2idx'][b] = len(dicts['labels2idx'])
+                lin = line.strip().split()
+                word = lin[0]
+                tag = lin[tag_position]
+                word = word.lower()
+                if word not in dicts['words2idx']:
+                    dicts['words2idx'][word] = len(dicts['words2idx'])
+                if tag not in dicts['labels2idx']:
+                    dicts['labels2idx'][tag] = len(dicts['labels2idx'])
+
 
     out = {}
     for type in ["train", "test", "valid"] :
         w = []
         l = []
-        o = []
         ww = []
         ll = []
-        oo = []
         with open(os.path.join(path, type + ".txt")) as f:
             for line in f:
                 if len(line.strip()) is 0 :
                     if len(w) > 0:
                         ww.append(w)
                         ll.append(l)
-                        oo.append(o)
                         w = []
                         l = []
-                        o = []
                     continue
-                a, b, c, d = line.strip().split()
-                a = a.lower()
-                w.append(dicts['words2idx'][a])
-                l.append(dicts['labels2idx'][b])
-                o.append(0)
-        out[type] = (ww, oo, ll)
+                lin = line.strip().split()
+                word = lin[0].lower()
+                tag = lin[tag_position]
+                w.append(dicts['words2idx'][word])
+                l.append(dicts['labels2idx'][tag])
+
+        out[type] = (ww, ll)
     return out["train"], out["valid"], out["test"], dicts
