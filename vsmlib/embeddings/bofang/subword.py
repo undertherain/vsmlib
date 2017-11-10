60e46aa54e46ccbd505e7ecf3146596c43e8ef24
bofang <libofangfang@163.com>
2017-11-10 14:47:21 +0900

    fixed a　is_pair_existing bug for CosAvg methods on analogy task

--- vsmlib/embeddings/bofang/subword.py
+++ vsmlib/embeddings/bofang/subword.py
@@ -5,13 +5,13 @@ Some essential functions for generating subword information
 import numpy as np
 
 
-def get_chars(index2word, maxWordLength) :
+def get_chars(index2word) :
     index2charIds = [[] for i, w in index2word.items()] # np.zeros((len(index2word), maxWordLength), dtype=np.int32) - 1
     vocab_char = {}
     for i, w in index2word.items():
         w_ = w + "@"
         charIds = []
-        for j in range(min(len(w_), maxWordLength)):
+        for j in range(min(len(w_))):
             # print(w[j] + " ")
             if w_[j] not in vocab_char:
                 vocab_char[w_[j]] = len(vocab_char)
